444f34d54584f601ce74d1478b2310c902941199
Marcin Pastuszak <marcin.pastuszak@student.put.poznan.pl>
2025-11-24 16:07:47 +0100

    Implement merge sort in Sorter class

--- src/main/java/pl/put/poznan/transformer/logic/Sorter.java
+++ src/main/java/pl/put/poznan/transformer/logic/Sorter.java
@@ -66,4 +66,50 @@ public class Sorter {
 
     }
 
+    private static void merge(double [] tab, int start, int end) {
+
+        double [] helper=new double[end-start+1];
+        if(end-start>=0)System.arraycopy(tab, start, helper, 0, end-start+1);
+
+        int middle=(start+end)/2;
+        int i=start;
+        int i_left=start;
+        int i_right=middle+1;
+        while(i_left<=middle && i_right<=end) {
+            if(helper[i_left-start]<helper[i_right-start]) {
+                tab[i]=helper[i_left-start];
+                i_left++;
+            }
+            else {
+                tab[i]=helper[i_right-start];
+                i_right++;
+            }
+            i++;
+        }
+        while(i_left<=middle) {
+            tab[i]=helper[i_left-start];
+            i_left++;
+            i++;
+        }
+        while(i_right<=end) {
+            tab[i]=helper[i_right-start];
+            i_right++;
+            i++;
+        }
+
+    }
+
+    private static void mergeSort(double [] tab, int start, int end) {
+
+        int middle=(start+end)/2;
+        if(start<middle) mergeSort(tab, start, middle);
+        if(middle+1<end) mergeSort(tab, middle+1, end);
+        merge(tab, start, end);
+
+    }
+
+    public static void mergeSort(double[] tab){
+        merge(tab, 0, tab.length-1);
+    }
+
 }
