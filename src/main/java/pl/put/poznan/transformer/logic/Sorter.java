f9b95eff9d4923ee235dec161870e566ed58c4c8
Marcin Pastuszak <marcin.pastuszak@student.put.poznan.pl>
2025-11-24 15:43:52 +0100

    Implement insertion sort in Sorter class

--- src/main/java/pl/put/poznan/transformer/logic/Sorter.java
+++ src/main/java/pl/put/poznan/transformer/logic/Sorter.java
@@ -4,16 +4,33 @@ public class Sorter {
 
     public static void bubbleSort(double [] tab) {
         boolean changed=false;
+
         for(int i=tab.length-1;i>0;i--) {
             for(int j=0; j<i; j++) {
                 if(tab[j]>tab[j+1]) {
                     double helper=tab[j+1];
                     tab[j+1]=tab[j];
                     tab[j]=helper;
+
                     changed=true;
                 }
             }
             if(!changed)return;
+        }
+    }
+
+    public static void insertSort(double [] tab) {
+        int n=tab.length-1;
+
+        for(int j=n-1;j>=0;j--) {
+
+            double x=tab[j];
+            int i=j+1;
+            while((i<=n)&&(x>tab[i])){
+                tab[i-1]=tab[i];
+                i++;
+            }
+            tab[i-1]=x;
         }
     }
 
