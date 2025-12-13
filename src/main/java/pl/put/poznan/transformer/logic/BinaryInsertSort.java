8b982d63dcddbbacb103f76e38e84ddc4f1a3dd4
Strolluj-Trolla <marty.pastuszak@gmail.com>
2025-12-13 15:28:49 +0100

    Modify selection and binary insert sort to allow control

--- src/main/java/pl/put/poznan/transformer/logic/BinaryInsertSort.java
+++ src/main/java/pl/put/poznan/transformer/logic/BinaryInsertSort.java
@@ -5,15 +5,20 @@ import static java.util.Objects.isNull;
 public class BinaryInsertSort implements SortAlgorithm{
     public void sort(Cell[][] tab, int column, int maxIter, Order order) {
         int n = tab.length - 1;
+        int curIter=0;
+        int sign=1;
+        if(order==Order.FALLING)sign=-1;
 
         if(isNull(tab[0][column].str)) {
             for (int j = n - 1; j >= 0; j--) {
+                if((curIter>=maxIter)&&(maxIter!=0))return;
+                curIter++;
                 Cell[] helper = tab[j];
                 int p = j;
                 int k = n + 1;
                 while (k - p > 1) {
                     int i = (p + k) / 2;
-                    if (helper[column].num <= tab[i][column].num) k = i;
+                    if (sign*helper[column].num <= sign*tab[i][column].num) k = i;
                     else p = i;
                 }
                 for (int i = j; i < p; i++) {
@@ -24,12 +29,14 @@ public class BinaryInsertSort implements SortAlgorithm{
         }
         else{
             for (int j = n - 1; j >= 0; j--) {
+                if((curIter>=maxIter)&&(maxIter!=0))return;
+                curIter++;
                 Cell[] helper = tab[j];
                 int p = j;
                 int k = n + 1;
                 while (k - p > 1) {
                     int i = (p + k) / 2;
-                    if (helper[column].str.compareTo(tab[i][column].str)<=0) k = i;
+                    if (sign*helper[column].str.compareTo(tab[i][column].str)<=0) k = i;
                     else p = i;
                 }
                 for (int i = j; i < p; i++) {
