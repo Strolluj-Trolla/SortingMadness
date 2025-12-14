db35a9433b1b17d5f5d3b17208fdd00920479c64
StanislawMarszalek <stanislawmarszalek890@gmail.com>
2025-12-14 19:31:37 +0000

    Deploying to javadoc from @ Strolluj-Trolla/SortingMadness@40ab0fd466b27aa346ece56ca06419afa7b4bef7 🚀

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.put.poznan.transformer.app"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.rest"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.put.poznan.transformer.app"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.rest"},{"l":"pl.put.poznan.transformer.rest.dto"}]
