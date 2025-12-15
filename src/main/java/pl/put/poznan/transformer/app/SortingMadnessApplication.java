2638b58cfef11fc12b1211eb8017ee2667f49c95
Marcin Pastuszak <marcin.pastuszak@student.put.poznan.pl>
2025-12-15 15:35:51 +0100

    Write javadoc for main apllication class

--- src/main/java/pl/put/poznan/transformer/app/SortingMadnessApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/SortingMadnessApplication.java
@@ -3,9 +3,18 @@ package pl.put.poznan.transformer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class for the SpringBoot application.
+ * Takes REST controllers from the <a href="#{@link}">@link pl.put.poznan.transformer.rest</a> package.
+ */
+
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class SortingMadnessApplication {
 
+    /**
+     * Main runner for the application.
+     * @param args Arguments to be passed o the apllication.
+     */
     public static void main(String[] args) {
         SpringApplication.run(SortingMadnessApplication.class, args);
     }
