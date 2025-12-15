313c2c0ae49ba885108f5ea153e539c788a4e569
Marcin Pastuszak <marcin.pastuszak@student.put.poznan.pl>
2025-12-15 11:55:42 +0100

    Simplify selection and binary insert sort

--- src/main/java/pl/put/poznan/transformer/logic/BinaryInsertSort.java
+++ src/main/java/pl/put/poznan/transformer/logic/BinaryInsertSort.java
@@ -1,7 +1,5 @@
 package pl.put.poznan.transformer.logic;
 
-import static java.util.Objects.isNull;
-
 public class BinaryInsertSort implements SortAlgorithm{
     public boolean sort(Cell[][] tab, int column, int maxIter, Order order) {
         int n = tab.length - 1;
@@ -9,41 +7,21 @@ public class BinaryInsertSort implements SortAlgorithm{
         int sign=1;
         if(order==Order.FALLING)sign=-1;
 
-        if(isNull(tab[0][column].str)) {
-            for (int j = n - 1; j >= 0; j--) {
-                if((curIter>=maxIter)&&(maxIter!=-1))return false;
-                curIter++;
-                Cell[] helper = tab[j];
-                int p = j;
-                int k = n + 1;
-                while (k - p > 1) {
-                    int i = (p + k) / 2;
-                    if (sign*helper[column].num <= sign*tab[i][column].num) k = i;
-                    else p = i;
-                }
-                for (int i = j; i < p; i++) {
-                    tab[i] = tab[i + 1];
-                }
-                tab[p] = helper;
+        for (int j = n - 1; j >= 0; j--) {
+            if((curIter>=maxIter)&&(maxIter!=-1))return false;
+            curIter++;
+            Cell[] helper = tab[j];
+            int p = j;
+            int k = n + 1;
+            while (k - p > 1) {
+                int i = (p + k) / 2;
+                if (sign*helper[column].compareTo(tab[i][column])<=0) k = i;
+                else p = i;
             }
-        }
-        else{
-            for (int j = n - 1; j >= 0; j--) {
-                if((curIter>=maxIter)&&(maxIter!=-1))return false;
-                curIter++;
-                Cell[] helper = tab[j];
-                int p = j;
-                int k = n + 1;
-                while (k - p > 1) {
-                    int i = (p + k) / 2;
-                    if (sign*helper[column].str.compareTo(tab[i][column].str)<=0) k = i;
-                    else p = i;
-                }
-                for (int i = j; i < p; i++) {
-                    tab[i] = tab[i + 1];
-                }
-                tab[p] = helper;
+            for (int i = j; i < p; i++) {
+                tab[i] = tab[i + 1];
             }
+            tab[p] = helper;
         }
         return true;
     }
--- src/main/java/pl/put/poznan/transformer/logic/SelectionSort.java
+++ src/main/java/pl/put/poznan/transformer/logic/SelectionSort.java
@@ -1,38 +1,21 @@
 package pl.put.poznan.transformer.logic;
 
-import static java.util.Objects.isNull;
-
 public class SelectionSort implements SortAlgorithm {
     public boolean sort(Cell[][] tab, int column, int maxIter, Order order) {
         int curIter=0;
         int sign=1;
         if(order==Order.FALLING)sign=-1;
 
-        if(isNull(tab[0][column].str)) {
-            for (int k = tab.length - 1; k > 0; k--) {
-                if((curIter>=maxIter)&&(maxIter!=-1))return false;
-                curIter++;
-                int selected = k;
-                for (int i = 0; i < k; i++) {
-                    if (sign*tab[i][column].num > sign*tab[selected][column].num) selected = i;
-                }
-                Cell[] helper = tab[selected];
-                tab[selected] = tab[k];
-                tab[k] = helper;
+        for (int k = tab.length - 1; k > 0; k--) {
+            if((curIter>=maxIter)&&(maxIter!=-1))return false;
+            curIter++;
+            int selected = k;
+            for (int i = 0; i < k; i++) {
+                if (sign*tab[i][column].compareTo(tab[selected][column])>0) selected = i;
             }
-        }
-        else{
-            for (int k = tab.length - 1; k > 0; k--) {
-                if((curIter>=maxIter)&&(maxIter!=-1))return false;
-                curIter++;
-                int selected = k;
-                for (int i = 0; i < k; i++) {
-                    if (sign*tab[i][column].str.compareTo(tab[selected][column].str)>0) selected = i;
-                }
-                Cell[] helper = tab[selected];
-                tab[selected] = tab[k];
-                tab[k] = helper;
-            }
+            Cell[] helper = tab[selected];
+            tab[selected] = tab[k];
+            tab[k] = helper;
         }
         return true;
     }
