56031025d9daa2e2966ba27d53a79f8f1d22f5a5
Marcin Pastuszak <marcin.pastuszak@student.put.poznan.pl>
2025-12-15 18:42:01 +0100

    Write javadoc for the SortResponseDTO class

--- src/main/java/pl/put/poznan/transformer/rest/dto/SortResponseDTO.java
+++ src/main/java/pl/put/poznan/transformer/rest/dto/SortResponseDTO.java
@@ -2,11 +2,21 @@ package pl.put.poznan.transformer.rest.dto;
 
 import java.util.List;
 
+/**
+ * Data Transfer Object containing the response from the application.
+ */
 public class SortResponseDTO {
     private List<SortResultDTO> results;
 
+    /**
+     * Constructor for making an empty {@link SortResponseDTO} object.
+     */
     public SortResponseDTO() { }
 
+    /**
+     * Constructor for making a ready {@link SortResponseDTO} object ready to be sent to the client.
+     * @param results a {@link List} of {@link SortResponseDTO} object containing results of performed tests.
+     */
     public SortResponseDTO(List<SortResultDTO> results) {
         this.results = results;
     }
