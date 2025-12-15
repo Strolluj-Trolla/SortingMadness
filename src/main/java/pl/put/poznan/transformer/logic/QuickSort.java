ae19b93d88892572408a7d66954d18d0a17ff76d
Marcin Pastuszak <marcin.pastuszak@student.put.poznan.pl>
2025-12-15 11:57:51 +0100

    Simplify quick sort

--- src/main/java/pl/put/poznan/transformer/logic/QuickSort.java
+++ src/main/java/pl/put/poznan/transformer/logic/QuickSort.java
@@ -1,7 +1,5 @@
 package pl.put.poznan.transformer.logic;
 
-import static java.util.Objects.isNull;
-
 public class QuickSort implements SortAlgorithm{
     public boolean sort(Cell[][] tab, int column, int maxIter, Order order) {
         return quickSort(tab, column, 0, tab.length-1, maxIter, order);
@@ -19,32 +17,17 @@ public class QuickSort implements SortAlgorithm{
         int sign=1;
         if(order==Order.FALLING)sign=-1;
 
-        if(isNull(tab[0][column].str)) {
-            do {
-                while (sign*tab[i][column].num < sign*pivot[column].num) i++;
-                while (sign*tab[j][column].num > sign*pivot[column].num) j--;
-                if (i <= j) {
-                    helper = tab[i];
-                    tab[i] = tab[j];
-                    tab[j] = helper;
-                    i++;
-                    j--;
-                }
-            } while (i <= j);
-        }
-        else{
-            do {
-                while (sign*tab[i][column].str.compareTo(pivot[column].str) < 0) i++;
-                while (sign*tab[j][column].str.compareTo(pivot[column].str) > 0) j--;
-                if (i <= j) {
-                    helper = tab[i];
-                    tab[i] = tab[j];
-                    tab[j] = helper;
-                    i++;
-                    j--;
-                }
-            } while (i <= j);
-        }
+        do {
+            while (sign*tab[i][column].compareTo(pivot[column]) < 0) i++;
+            while (sign*tab[j][column].compareTo(pivot[column]) > 0) j--;
+            if (i <= j) {
+                helper = tab[i];
+                tab[i] = tab[j];
+                tab[j] = helper;
+                i++;
+                j--;
+            }
+        } while (i <= j);
 
         boolean complete=true;
         if (i < end) complete=quickSort(tab, column, i, end, newMax, order);
